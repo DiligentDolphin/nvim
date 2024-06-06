f088593f6a1392bf9077a7bd9c9f129e85a37dae
Allen S. Paul <allen.s.paul@outlook.com>
2024-06-06 22:28:17 +0800

    keymap for cmdline-mode

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,1 +1,9 @@
 local keymap = vim.keymap
+-- cmdline tcsh-style
+keymap.set( 'c', '<C-A>', '<HOME>' )
+keymap.set( 'c', '<C-E>', '<END>' )
+keymap.set( 'c', '<C-F>', '<Right>' )
+keymap.set( 'c', '<C-B>', '<Left>' )
+keymap.set( 'c', '<Esc>b', '<S-Left>' )
+keymap.set( 'c', '<Esc>f', '<S-Right>' )
+-- vim:tw=2:sw=2:et
