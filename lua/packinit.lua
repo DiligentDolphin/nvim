e27b52d1e472d8cbbfe881e99357111e719b3f04
Allen S. Paul <allen.s.paul@outlook.com>
2024-06-07 16:16:27 +0800

    use which-key plugin to suggest default keymap

--- lua/packinit.lua
+++ lua/packinit.lua
@@ -14,10 +14,25 @@ end
 vim.opt.rtp:prepend(lazypath)
 -- Load and setup
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  { "nvim-treesitter/nvim-treesitter", 
+  { 
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+    },
+  },
+  { 
+    "folke/neoconf.nvim",
+    cmd = "Neoconf",
+  },
+  { 
+    "folke/neodev.nvim",
+  },
+  { 
+    "nvim-treesitter/nvim-treesitter", 
     build = ":TSUpdate" ,
     config = function ()
       local configs = require("nvim-treesitter.configs")
@@ -34,54 +49,11 @@ require("lazy").setup({
       vim.opt.foldenable=false
     end
   },
-  { 'numToStr/Comment.nvim',
+  { 
+    'numToStr/Comment.nvim',
     opts = {
       -- add any options here
     },
-    lazy = false,
-    config = function ()
-      local configs = require("Comment")
-
-      configs.setup({
-        ---Add a space b/w comment and the line
-        padding = true,
-        ---Whether the cursor should stay at its position
-        sticky = true,
-        ---Lines to be ignored while (un)comment
-        ignore = nil,
-        ---LHS of toggle mappings in NORMAL mode
-        toggler = {
-          ---Line-comment toggle keymap
-          line = 'gcc',
-          ---Block-comment toggle keymap
-          block = 'gbc', },
-        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-        opleader = {
-          ---Line-comment keymap
-          line = 'gc',
-          ---Block-comment keymap
-          block = 'gb', },
-        ---LHS of extra mappings
-        extra = {
-          ---Add comment on the line above
-          above = 'gcO',
-          ---Add comment on the line below
-          below = 'gco',
-          ---Add comment at the end of line
-          eol = 'gcA', },
-        ---Enable keybindings NOTE: If given `false` then the plugin won't
-        ---create any mappings
-        mappings = {
-          ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}`
-          ---`gb[count]{motion}`
-          basic = true,
-          ---Extra mapping; `gco`, `gcO`, `gcA`
-          extra = true, },
-        ---Function to call before (un)comment
-        pre_hook = nil,
-        ---Function to call after (un)comment
-        post_hook = nil, }) 
-    end 
   },
 })
 -- vim:ts=2:sw=2:et:
