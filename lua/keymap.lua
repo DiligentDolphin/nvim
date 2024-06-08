fdb922d8c2d30e137f53ddadb120b55c2dc2996d
Allen S. Paul <allen.s.paul@outlook.com>
2024-06-08 20:50:50 +0800

    delete tcsh-style keymap in cmd mode

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,9 +1,1 @@
 local keymap = vim.keymap
--- cmdline tcsh-style
-keymap.set( 'c', '<C-A>', '<HOME>' )
-keymap.set( 'c', '<C-E>', '<END>' )
-keymap.set( 'c', '<C-F>', '<Right>' )
-keymap.set( 'c', '<C-B>', '<Left>' )
-keymap.set( 'c', '<Esc>b', '<S-Left>' )
-keymap.set( 'c', '<Esc>f', '<S-Right>' )
-keymap.set( 'c', '<C-D>', '<Del>' )
