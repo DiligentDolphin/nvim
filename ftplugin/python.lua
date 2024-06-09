a3d73950642d057d40e8948d2d01a7662551fd51
Allen S. Paul <allen.s.paul@outlook.com>
2024-06-09 14:58:12 +0800

    add lua lsp

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,5 +1,5 @@
 vim.lsp.start({
-	name = 'python',
-	cmd = {'pylsp'},
-	root_dir = vim.fs.root(0, {'pyproject.toml', 'setup.py'}),
+	name = 'python-language-server',
+	cmd = { 'pylsp' },
+	root_dir = vim.fs.root(0, { 'pyproject.toml', 'setup.py' }),
 })
