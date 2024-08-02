3c22cabd933744092d28c72da2381bfe77919607
Allen S. Paul <allen.s.paul@outlook.com>
2024-08-02 22:09:54 +0800

    use findfile to expand user home directory

--- lua/profile.lua
+++ lua/profile.lua
@@ -8,8 +8,8 @@ opt.smartindent = true
 opt.smarttab = true
 opt.autochdir = true
 cmd [[
-let prog_python3='~/.pyenv/versions/3.12.4/bin/python'
-if findfile(prog_python3) != ''
+let prog_python3=findfile('~/.pyenv/versions/3.12.4/bin/python')
+if prog_python3 != ''
 	let g:python3_host_prog = prog_python3
 endif
 ]]
