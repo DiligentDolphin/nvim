48f306ac44c64fef217b678ef5751231fca429b5
Allen S. Paul <allen.s.paul@outlook.com>
2024-08-02 22:03:52 +0800

    set python_prog when exists

--- lua/profile.lua
+++ lua/profile.lua
@@ -7,5 +7,12 @@ opt.smartcase = true
 opt.smartindent = true
 opt.smarttab = true
 opt.autochdir = true
+cmd [[
+let prog_python3='~/.pyenv/versions/3.12.4/bin/python'
+if findfile(prog_python3) != ''
+	let g:python3_host_prog = prog_python3
+endif
+]]
+
 
 -- vim: ts=2 sts=2 sw=2
