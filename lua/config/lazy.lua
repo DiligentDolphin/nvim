599bba687ad80e0a706f63d3caf31a25006e0990
Allen S. Paul <allen.s.paul@outlook.com>
2024-08-04 22:56:51 +0800

    vscode compat

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,8 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", cond = not vim.g.vscode },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins.extras.vscode", cond = vim.g.vscode },
     -- import/override with your plugins
     { import = "plugins" },
   },
