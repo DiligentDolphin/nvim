7e87a1723ef16f0257b5fd3855c3a1228e560eb1
diligentdolphin <allen.s.paul@outlook.com>
2024-08-16 13:40:53 +0800

    set shell to pwsh on Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local function is_windows()
+  return vim.fn.has("win64") or vim.fn.has("win32") or vim.fn.has("win16")
+end
+
+if is_windows() and vim.fn.executable("pwsh") then
+  vim.o.shell = "pwsh"
+end
