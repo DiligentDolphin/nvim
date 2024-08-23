b45f472814ce5999aef003ac33f27a5cd6ecdf4b
diligentdolphin <allen.s.paul@outlook.com>
2024-08-23 08:40:32 +0800

    configure: custom conda envs directory
    on office laptop

--- lua/plugins/venv_selector.lua
+++ lua/plugins/venv_selector.lua
@@ -8,4 +8,13 @@ return {
   },
   lazy = false,
   branch = "regexp", -- This is the regexp branch, use this for the new version
+  opts = {
+    settings = {
+      search = {
+        -- configure: custom conda envs directory on office laptop
+        conda_ol = { command = "fd python\\.exe d:\\\\conda\\\\envs" },
+        cond = vim.uv.os_uname().sysname == "Windows_NT" and vim.fn.isdirectory("d:\\\\conda\\\\envs"),
+      },
+    },
+  },
 }
